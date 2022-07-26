be01dabc12b5966c4574245da4eae0c706b45fc8
kisperal <stirmanjr@gmail.com>
2022-07-26 15:38:00 -0400

    Changes

--- build.lua
+++ build.lua
@@ -2,13 +2,18 @@ local dir = remodel.readDir("songs")
 local md5 = require("packages/md5")
 
 local maps = Instance.new("Model")
-local songMaps = Instance.new("Folder")
 local songMetadata = Instance.new("ModuleScript")
+local songMaps = Instance.new("Folder")
+local songDifficulties = Instance.new("ModuleScript")
+
 maps.Name = "Songs"
 songMaps.Name = "SongMaps"
 songMetadata.Name = "SongMetadata"
+songDifficulties.Name = "SongDifficulties"
+
 songMaps.Parent = maps
 songMetadata.Parent = maps
+songDifficulties.Parent = maps
 
 print("Building maps for RoBeats CS")
 
@@ -24,9 +29,43 @@ end
 
 local metadataOut = {
     "local SongMaps = script.Parent.SongMaps",
+    "local SongDifficulties = require(script.Parent.SongDifficulties)",
     "",
     "return {"
 }
+
+local function concat(tbl, sep)
+    local str = ""
+    for i, v in ipairs(tbl) do
+        str = str .. v .. sep
+    end
+    return str
+end
+
+local function serializeTable(table, level)
+    local indent = string.rep("\t", level)
+    local out = {}
+
+    for k, v in pairs(table) do
+        local serKey
+
+        if type(k) == "string" then
+            serKey = "[\"" .. tostring(k) .. "\"]"
+        else
+            serKey = "[" .. tostring(k) .. "]"
+        end
+
+        if type(v) == "table" then
+            out[#out + 1] = indent .. serKey .. " = " .. serializeTable(v, level + 1)
+        else
+            out[#out + 1] = indent .. serKey .. " = " .. tostring(v)
+        end
+    end
+
+    return "{\n" .. concat(out, ",\n") .. string.rep("\t", level - 1) .. "}"
+end
+
+local mapDifficulties = {}
 
 for i, file in ipairs(dir) do
     local jsonString
@@ -38,15 +77,18 @@ for i, file in ipairs(dir) do
     end)
 
     if type(mapData) == "table" then
+        local difficulties = mapData.AudioDifficulty
+
         if suc then
             mapData.AudioMapData = string.format("###SongMaps:FindFirstChild(\"%s\")", file):gsub(".json", "")
+            mapData.AudioDifficulty = string.format("###SongDifficulties[\"%s\"]", mapData.AudioMD5Hash)
 
             local object = {}
 
             table.insert(object, "\t{")
 
             for property, value in pairs(mapData) do
-                if property ~= "HitObjects" then
+                if property ~= "HitObjects" and property ~= "NpsGraph" and property ~= "TimingPoints" then
                     local propertyLine = ""
 
                     if type(value) == "string" then
@@ -59,6 +101,8 @@ for i, file in ipairs(dir) do
                         end
                     elseif type(value) == "number" then
                         propertyLine = string.format("\t\t%s = %s,", property, value)
+                    elseif type(value) == "table" then
+                        propertyLine = string.format("\t\t%s = %s,", property, serializeTable(value, 3))
                     end
                         
                     if propertyLine ~= "" then
@@ -94,6 +138,8 @@ for i, file in ipairs(dir) do
             remodel.setRawProperty(mapDataValueObject, "Value", "String", split)
         end
 
+        mapDifficulties[mapData.AudioMD5Hash] = difficulties
+
         print(string.format("Built song %d out of %d (%0.2f%% complete)", i, #dir, (i / #dir) * 100))
     else
         print(string.format("Failed to build song %s! (%0.2f%% complete)", file, (i / #dir) * 100))
@@ -102,5 +148,6 @@ end
 
 table.insert(metadataOut, "}")
 
+remodel.setRawProperty(songDifficulties, "Source", "String", string.format("return %s", serializeTable(mapDifficulties, 1)))
 remodel.setRawProperty(songMetadata, "Source", "String", table.concat(metadataOut, "\n"))
 remodel.writeModelFile(maps, "out.rbxmx")
